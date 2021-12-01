fb29eda3b34e91ca6f6fbbe8b03069eebebd1a86
hasan <rampurawalahasan4@gmail.com>
2021-12-01 22:22:13 +0530

    shows which directories files were added in.

--- files_sorter.py
+++ files_sorter.py
@@ -10,6 +10,15 @@ class SortFiles:
     def __init__(self, base_dir):
         self.number_of_files_moved = 0
         self.base_dir = base_dir
+        patterns, default_dir, end_dirs = self.get_config()
+        self. patterns = patterns
+        self.default_dir = default_dir
+        self.end_dirs = end_dirs
+        arr = []
+        for x in self.end_dirs:
+            arr.append(0)
+
+        self.to_move_dirs_dict = dict(zip(self.end_dirs, arr))
 
     def move_file(self, pattern, filename, end_dir):
         base_dir = self.base_dir
@@ -26,28 +35,29 @@ class SortFiles:
             final_destination = os.path.join(base_dir, end_dir, date.today().strftime('%d-%m-%Y') + f'_{filename}')
             shutil.move(original_dest, final_destination)
 
-        elif pattern == '.exe':
-            final_destination = os.path.join(base_dir, end_dir, f'_{filename}')
+        elif pattern == '.appimage' or '.AppImage' or '.deb':
+            final_destination = os.path.join(base_dir, end_dir, f'{filename}')
             shutil.move(original_dest, final_destination)
         print(final_destination)
         print(f'file moved to {end_dir} !')
+        end_dir = end_dir.replace('/', '')
+        self.to_move_dirs_dict[end_dir] += 1
 
     def get_pattern_matches(self, directories):
-        patterns, default_dir, end_dirs  = self.get_config()
         for src_path in directories:
             index = 0
-            for pattern in patterns:
+            for pattern in self.patterns:
                 start_str, end_str = pattern
                 if start_str is not None:
                     matches = re.search(f'^{start_str}.*{end_str}$', src_path)
                     if matches:
-                        end_dir = end_dirs[index] + '/'
+                        end_dir = self.end_dirs[index] + '/'
                         self.move_file(end_str, src_path, end_dir)
                         self.number_of_files_moved += 1
                 else:
                     matches = re.search(f' *{end_str}$', src_path)
                     if matches:
-                        end_dir = end_dirs[index] + '/'
+                        end_dir = self.end_dirs[index] + '/'
                         self.move_file(end_str, src_path, end_dir)
                         self.number_of_files_moved += 1
                 index += 1
@@ -66,4 +76,11 @@ directories = os.listdir(base_dir)
 files_sorter.get_pattern_matches(directories=directories)
 print(f'\n\nnumber of files moved: {files_sorter.number_of_files_moved}')
 time_taken = time.time() - start_time
+
+added_folders_dict = files_sorter.to_move_dirs_dict
+for key in added_folders_dict.keys():
+    n_of_files = added_folders_dict[key]
+    if n_of_files != 0:
+        print(f'files added in {key}: {n_of_files}')
+
 print(f'finished in {time_taken} seconds')
